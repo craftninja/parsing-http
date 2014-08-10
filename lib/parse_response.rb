e1fc7ba988a00562b2c743d1a8d3c4cc1db538a8
EmilyPlatzer <emilyplatzer@gmail.com>
2014-08-09 18:54:58 -0600

    Rename variable to be more descriptive, reduce duplication

--- lib/parse_response.rb
+++ lib/parse_response.rb
@@ -1,25 +1,24 @@
 class HTTPResponseParser
 
-  def initialize(something)
-    @something = something
+  def initialize(response)
+    @response = response.split("\n")
   end
 
   def response_code
-    @something.split("\n").first[9..11].to_i
+    @response.first[9..11].to_i
   end
 
   def version
-    @something.split("\n").first[0..7]
+    @response.first[0..7]
   end
 
   def body
-    response = @something.split("\n")
-    response[9..response.length].join
+    @response[9..@response.length].join
   end
 
   def header
     header = {}
-    header_list = @something.split("\n")[1..7]
+    header_list = @response[1..7]
     header_list.each do |component|
       parts = component.split(": ")
       header["#{parts[0]}:"] = parts[1]
